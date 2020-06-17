8b9c6a6a247e1cb294a0cf7531b66679a44c49d3
Kharisma Putra <ks.dwiputra@gmail.com>
2020-06-17 12:42:06 +0800

    Updating schema for Product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_17_041011) do
+ActiveRecord::Schema.define(version: 2020_06_17_041715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,15 @@ ActiveRecord::Schema.define(version: 2020_06_17_041011) do
     t.string "province"
     t.string "postal_code"
     t.string "phone"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "code"
+    t.string "name"
+    t.float "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
