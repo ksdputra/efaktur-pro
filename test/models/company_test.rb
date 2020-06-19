fa393b1a23b092119ea97261e6e13da3b1ffbc19
Kharisma Putra <ks.dwiputra@gmail.com>
2020-06-19 23:46:41 +0800

    Add test for Company and User models

--- test/models/company_test.rb
+++ test/models/company_test.rb
@@ -1,7 +1,54 @@
 require 'test_helper'
 
 class CompanyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @company = Company.new(
+      user_id: User.first.id,
+      npwp: '123456789012345',
+      name: 'PT Berdikari',
+      address: 'Taman Berdikari Sentosa',
+      block: 'H',
+      number: '3-5',
+      rt: '001',
+      rw: '001',
+      village: 'Pejaten',
+      subdistrict: 'Cikarang',
+      regency: 'Jakarta Timur',
+      province: 'DKI Jakarta',
+      postal_code: '123456',
+      phone: '021234567',
+      is_dropdown: false
+    )
+  end
+
+  test 'valid company' do
+    assert @company.valid?
+  end
+
+  test 'invalid npwp' do
+    @company.npwp = nil
+    refute @company.valid?, 'company saved without npwp'
+
+    @company.npwp = '12345'
+    refute @company.valid?, 'company saved with npwp less than 15 characters'
+
+    @company.npwp = '1234567890123456'
+    refute @company.valid?, 'company saved with npwp more than 15 characters'
+  end
+
+  test 'invalid name' do
+    @company.name = nil
+    refute @company.valid?, 'company saved without name'
+
+    @company.name = 'a' * 2
+    refute @company.valid?, 'company saved with name less than 3 characters'
+
+    @company.name = 'a' * 51
+    refute @company.valid?, 'company saved with name more than 50 characters'
+  end
+
+  test 'invalid without address' do
+    @company.address = nil
+    refute @company.valid?
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,48 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(
+      npwp: '123456789012345',
+      email: 'kputra@mailinator.com',
+      password: '123456'
+    )
+  end
+
+  test 'valid user' do
+    assert @user.valid?
+  end
+
+  test 'invalid npwp' do
+    @user.npwp = nil
+    refute @user.valid?, 'user saved without npwp'
+
+    @user.npwp = '12345'
+    refute @user.valid?, 'user saved with npwp less than 15 characters'
+
+    @user.npwp = '1234567890123456'
+    refute @user.valid?, 'user saved with npwp more than 15 characters'
+  end
+
+  test 'invalid email' do
+    @user.email = nil
+    refute @user.valid?, 'user saved without email'
+
+    @user.email = 'kputramailinator.com'
+    refute @user.valid?, 'user save without the right format'
+
+    @user.email = 'kputra@mailinator'
+    refute @user.valid?, 'user save without the right format'
+  end
+
+  test 'invalid password' do
+    @user.password = nil
+    refute @user.valid?, 'user saved without password'
+
+    @user.name = 'a' * 5
+    refute @user.valid?, 'user saved with name less than 6 characters'
+
+    @user.name = 'a' * 31
+    refute @user.valid?, 'user saved with name more than 30 characters'
+  end
 end
